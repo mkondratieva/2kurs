a560f280b5b500811367983531bd092b8b1f38a6
marina kondratieva <55560721+mkondratieva@users.noreply.github.com>
2025-09-13 16:22:37 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+class C{ //класс комплексное число
+	double Re,Im; 
+	public:
+	C(double Re, double Im){
+		this->Re=Re;this->Im=Im;
+	} //конструктор с параметрами
+	C operator +(const C &z){
+		return C(Re+z.Re, Im+z.Im);
+	}//оператор сложения
+	C &sopr(){  //сопряжение
+		Im=-Im;
+		return(*this);
+	} 
+	friend ostream &operator <<(ostream &stream, const C &obj){
+		stream<<obj.Re<<"+i"<<obj.Im<<" ";
+		return stream;
+	}
+	 /*	
+	//? operator ++(){......}//префиксный ++
+	//? operator ++(int){.....} //постпрефиксный ++
+	 */
+
+};
+int main(){ 
+
+	cout<<(C(-9.,7.)+C(9.,-7.));
+	cout<<C(8,8).sopr();
+	C z(1,2);
+//	cout<<z<<++z<<z<<z++<<z;     //тест инкремента
+	return 0;
+}
+	
